6af3b89103c006be0759401bb360832f68c46895
Mark Conroy <mark@mark.ie>
2021-02-10 21:28:06 +0100

    creates a page per sheet entry

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,34 +7,34 @@
 import path from 'path'
 
 // Grab list of dev workspaces and create pages from them.
-// async function turnDevWorkspacesIntoPages({ graphql, actions }) {
-//   // 1. Get a template for this page
-//   const devWorkspacesTemplate = path.resolve(
-//     './src/templates/DevWorkspaces.js'
-//   )
-//   // 2. Query all blogs
-//   const { data } = await graphql(`
-//     query {
-//       devWorkspaces: allNodeBlog {
-//         nodes {
-//           path {
-//             alias
-//           }
-//         }
-//       }
-//     }
-//   `)
-//   // 3. Loop over each blog and create a page for it.
-//   data.devWorkspaces.nodes.forEach(devWorkspaces => {
-//     actions.createPage({
-//       path: `${blog.path.alias}`,
-//       component: blogPostTemplate,
-//       context: {
-//         slug: blog.path.alias,
-//       },
-//     })
-//   })
-// }
+async function turnDevWorkspacesIntoPages({ graphql, actions }) {
+  // 1. Get a template for this page
+  const devWorkspacesTemplate = path.resolve(
+    './src/templates/DevWorkspace.js'
+  )
+  // 2. Query all blogs
+  const { data } = await graphql(`
+    query {
+      devWorkspaces: allGoogleSheetDevWorkspacesRow {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `)
+  // 3. Loop over each blog and create a page for it.
+  data.devWorkspaces.edges.forEach(devWorkspace => {
+    actions.createPage({
+      path: `${devWorkspace.node.id}`,
+      component: devWorkspacesTemplate,
+      context: {
+        slug: `${devWorkspace.node.id}`,
+      },
+    })
+  })
+}
 
 export async function createPages(params) {
   // Create pages dynamically
